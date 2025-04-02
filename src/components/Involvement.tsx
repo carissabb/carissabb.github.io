8f749519a90d49cec912b4f6f30c70c8b8392d18
Carissa B Bostian <email4cbb@gmail.com>
2025-04-02 10:39:25 -0400

    added involvement imgs

--- src/components/Involvement.tsx
+++ src/components/Involvement.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import cybervtImg from '../assets/cybervt.jpg';
+import girlsWhoCodeImg from '../assets/gwc.png';
+import libertyImg from '../assets/libertyLogo.png';
+import luCyberImg from '../assets/luCyber.png';
 
 export const Involvement = () => {
   const involvement = [
@@ -32,6 +36,15 @@ export const Involvement = () => {
     }
   ]
 
+  const involvementImages: { [key: string]: string } = {
+    "CyberVT": cybervtImg,
+    "Girls Who Code LU": girlsWhoCodeImg,
+    "C++ Tutor": libertyImg,
+    "Cybersecurity Club": luCyberImg,
+  };
+
+
+
   return (
     <div className="bg-white py-24 sm:py-32" id="involvement">   
 
@@ -45,16 +58,24 @@ export const Involvement = () => {
         <div className="mx-auto mt-16 max-w-7xl">
           <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
             {involvement.map((involvement, idx) => (
-              <div key={idx} className="bg-white rounded-2xl p-6 shadow-lg hover:shadow-xl transition-shadow border border-gray-100">
-                <h3 className="text-lg font-semibold text-gray-900">{involvement.title}</h3>
-                        <div className="px-4 py-2">
-                            <h3 className="text-sm font-semibold leading-6 text-gray-900"><em>{involvement.role}</em></h3>
-                            <p className="text-sm text-gray-500">{involvement.period}</p>
-                        </div>
-                    <p className="text-sm text-gray-500">{involvement.association} | {involvement.location}</p>
-                <div className="flex flex-wrap gap-2">
-                    
+              <div key={idx} className="bg-white rounded-2xl p-6 shadow-lg hover:shadow-xl transition-shadow border border-gray-100 flex items-center justify-between">
+                <div>
+                  <h3 className="text-lg font-semibold text-gray-900">{involvement.title}</h3>
+                  <div className="px-4 py-2">
+                    <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                      <em>{involvement.role}</em>
+                    </h3>
+                    <p className="text-sm text-gray-500">{involvement.period}</p>
+                  </div>
+                  <p className="text-sm text-gray-500">
+                    {involvement.association} | {involvement.location}
+                  </p>
                 </div>
+                <img
+                  src={involvementImages[involvement.title]}
+                  alt={`${involvement.title} Logo`}
+                  className="w-16 h-16 object-contain rounded-lg"
+                />
               </div>
             ))}
           </div>
